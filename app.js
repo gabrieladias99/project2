3c1dbc02e3083503329348a766c418693d5a67b5
Gabriela Dias <gabrieladias.souza@hotmail.com>
2019-07-19 15:08:22 -0300

    FInally done

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
-app.locals.title = 'Appzinho do Dax e da Gaby';
+app.locals.title = 'GIG';
 
 // express-session configuration
 app.use(session({
